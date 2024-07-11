196c5118f8b6b8ff126fb00571424fff5d817ec1
Inpoina <140089365+Inpoina@users.noreply.github.com>
2024-07-11 17:59:21 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ apt upgrade
 mv ~/idm/id.bin ~/id.bin
 rm -rf idm
 git clone https://github.com/Inpoina/idm.git
-mv *.bin ~/idm/id.bin
+mv id.bin ~/idm/id.bin
 echo "================================="
 echo "sukses"
