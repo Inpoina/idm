0f3e04df1353449e35fd5813c7f25fc63e4cf143
Inpoina <140089365+Inpoina@users.noreply.github.com>
2024-09-14 19:08:34 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,4 +1,17 @@
-cat fix.sh
+# Cek apakah zip sudah terinstal
+if ! command -v zip &> /dev/null
+then
+    echo "zip tidak ditemukan, menginstal..."
+    pkg install zip
+else
+    echo "zip installed"
+fi
+
+# Membuat file zip dengan nama fix.zip dari semua file di folder ~/storage/shared/Kampret/domar/
 zip -j fix.zip ~/storage/shared/Kampret/domar/*
 
+# Memindahkan file fix.zip ke folder ~/storage/shared/Kampret/
 mv *.zip ~/storage/shared/Kampret/
+
+# Tampilkan pesan setelah selesai
+echo "Proses selesai."
