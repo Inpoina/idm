e26faaf2d2a8c7a501a1019c25ae2ec519910e73
Inpoina <140089365+Inpoina@users.noreply.github.com>
2025-02-15 18:51:55 +0700

    Create cekstok.sql

---
+++ cekstok.sql
@@ -0,0 +1,13 @@
+SELECT 
+   
+    t1.plu,
+    t1.deskripsi,
+    COALESCE(t2.qty, 0) AS stok_awal,
+    COALESCE(SUM(t3.qty), 0) AS stok_keluar,
+    COALESCE(t2.qty, 0) - COALESCE(SUM(t3.qty), 0) AS stok_akhir
+FROM list t1
+LEFT JOIN stok t2 ON t1.plu = t2.plu
+LEFT JOIN sold t3 ON t1.plu = t3.plu
+GROUP BY t1.id, t1.plu, t1.deskripsi, t2.qty
+ORDER BY t1.id;
+EOF
