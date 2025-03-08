402e161a9018c4008b41dcb54fcdfe6faae3addd
Inpoina <140089365+Inpoina@users.noreply.github.com>
2025-03-09 05:33:50 +0700

    Add files via upload

---
+++ updprice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Move the file
+mv ~/storage/shared/kampret/UGD_PRODMAST.csv ~/idm/ 
+
+# Run MariaDB commands
+mariadb -u root -proot <<EOF
+use idm;
+delete from produk;
+LOAD DATA LOCAL INFILE '/data/data/com.termux/files/home/idm/UGD_PRODMAST.csv'
+INTO TABLE produk
+FIELDS TERMINATED BY '|'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+EOF
---
+++ updstok.sh
@@ -0,0 +1,12 @@
+rm UGD_STMAST*
+mv ~/storage/shared/kampret/UGD_STMAST.csv ~/idm/
+
+mariadb -u root -proot <<EOF
+use idm;
+delete from stok;
+LOAD DATA LOCAL INFILE '/data/data/com.termux/files/home/idm/UGD_STMAST.csv'
+INTO TABLE stok
+FIELDS TERMINATED BY '|'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+EOF
