e2092518afe53b322a4231692f5088bb6809d3eb
Inpoina <140089365+Inpoina@users.noreply.github.com>
2025-07-18 03:04:13 +0700

    Update updpromo.sh

--- updpromo.sh
+++ updpromo.sh
@@ -4,8 +4,8 @@
 
 mv ~/storage/shared/kampret/query/UGD_promo_matriks.csv ~/storage/shared/kampret/UGD_promo_matriks.csv
 mv ~/storage/shared/kampret/UGD_promo_matriks.csv ~/idm/
-
-# Run MariaDB commands                                                      mariadb -u root -proot <<EOF
+#run maria db
+mariadb -u root -proot <<EOF
 use idm;
 delete from produk;
 LOAD DATA LOCAL INFILE '/data/data/com.termux/files/home/idm/UGD_PRODMAST.csv'
